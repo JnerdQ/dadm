236b76d5758de1fbb09711c31019fccdb0eddfbe
JnerdQ <julyanyto@gmail.com>
2024-10-29 18:10:51 -0500

    Hola mundo en Kotlin

---
+++ app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.holamundo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.holamundo.ui.theme.HolaMundoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HolaMundoTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Greeting(name = "Mundo")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(Color.Black)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Hola $name!",
+            color = Color.White
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {g
+    HolaMundoTheme {
+        Greeting("Android")
+    }
+}
